8ac91308538601b86af098fc2ccfed1d32fe5605
bmauser <bmauser@gmail.com>
2015-02-04 03:20:21 +0100

    -

---
+++ Psa_Functions_Test.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once 'psa_init.php';
+
+
+class Psa_Functions_Test extends PHPUnit_Framework_TestCase{
+
+
+	protected function setUp(){
+
+		run_sql_file();
+	}
+
+
+	public function testCFG(){
+
+		Psa_
+		
+		
+		$this->assertEquals('psa', $user->username);
+		
+	}
+
+
+}
