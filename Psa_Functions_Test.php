773011e206165ba823bb147122ceb0a336c4461b
bmauser <bmauser@gmail.com>
2015-02-05 02:56:38 +0100

    -

--- Psa_Functions_Test.php
+++ Psa_Functions_Test.php
@@ -6,19 +6,67 @@ include_once 'psa_init.php';
 class Psa_Functions_Test extends PHPUnit_Framework_TestCase{
 
 
-	protected function setUp(){
+	//protected function setUp(){
 
-		run_sql_file();
+	//	run_sql_file();
+	//}
+	
+	public function testPSA_CFG1(){
+
+		$cfg = PSA_CFG();
+		$this->assertArrayHasKey('logging', $cfg);
+		
+		$cfg = &PSA_CFG();
+		$cfg['test1'] = 123;
+		$this->assertEquals(PSA_CFG()['test1'], 123);
+		
+		$cfg = &PSA_CFG();
+		$cfg['test2']['test3'] = 456;
+		$this->assertEquals(PSA_CFG('test2.test3'), 456);
+		
+		$cfg = &PSA_CFG('test2.test3');
+		$cfg = 789;
+		$this->assertEquals(PSA_CFG('test2.test3'), 789);
+		$this->assertEquals(PSA_CFG()['test2']['test3'], 789);
+		
+		PSA_CFG()['test4']['test4'] = 777;
+		$this->assertEquals(PSA_CFG('test4.test4'), 777);
+		
+	}
+	
+	
+	/**
+	 * @expectedException PSA_CFG_Exception
+	 */
+	public function testPSA_CFG2(){
+	
+		$cfg = PSA_CFG();
+		$this->assertArrayHasKey('logging', $cfg);
+		$cfg['eeee'] = 123;
+		PSA_CFG('eeee');
 	}
 
 
-	public function testCFG(){
+	public function testPSA_CFG3(){
 
-		Psa_
+		$arr['aaa'] = 1;
+		$arr['bb']['cc'] = 2;
+		$arr['bb']['oo'] = new stdClass();
+		$arr['bb']['oo']->gg = 3;
+		$arr['bb']['oo']->uu = 4;
+		$arr['bb']['oo']->pp = array('ee' => 5);
+		$arr['qq'] = new stdClass();
+		$arr['qq']->ww = new stdClass();
+		$arr['qq']->ww->uu = 6;
 		
+		PSA_CFG()['test'] = $arr;
 		
-		$this->assertEquals('psa', $user->username);
-		
+		$this->assertEquals(PSA_CFG('test.aaa'), 1);
+		$this->assertEquals(PSA_CFG('test.bb.cc'), 2);
+		$this->assertEquals(PSA_CFG('test.bb.oo->gg'), 3);
+		$this->assertEquals(PSA_CFG('test.bb.oo->uu'), 4);
+		$this->assertEquals(PSA_CFG('test.bb.oo->pp.ee'), 5);
+		$this->assertEquals(PSA_CFG('test.qq->ww->uu'), 6);		
 	}
 
 
